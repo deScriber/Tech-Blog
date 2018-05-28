1643583a2689c91b02cf2332dc62b683eab9d4d2
deScriber <tholsclaw@protonmail.com>
2018-05-28 12:16:46 -0700

    Pythonorate!

--- build.py
+++ build.py
@@ -1,23 +1,77 @@
-read_top = open("templates/top.html").read()
-read_mid = open("content/index.html").read()
-read_bottom = open("templates/bottom.html").read()
-read_file = read_top + read_mid + read_bottom
-open("docs/index.html", "w+").write(read_file)
+page_name = [
+    {
+        "filename": "content/index.html",
+        "title": "Index"
+    }
+    {
+        "filename": "content/blog.html",
+        "title": "Blog"
+    }
+    {
+        "filename": "content/projects.html",
+        "title": "Projects"
+    }
+    {
+        "filename": "content/connect.html",
+        "title": "Connect"
+    }
+]
 
-read_top = open("templates/top.html").read()
-read_mid = open("content/blog.html").read()
-read_bottom = open("templates/bottom.html").read()
-read_file = read_top + read_mid + read_bottom
-open("docs/blog.html", "w+").write(read_file)
+def main():
+    read_top = open('templates/top.html').read()
+    read_mid = open("content/index.html").read()
+    read_bottom = open("templates/bottom.html").read()
+    read_file = read_top + read_mid + read_bottom
+    open("docs/index.html", "w+").write(read_file)
+    read_top = open("templates/top.html").read()
+    read_mid = open("content/blog.html").read()
+    read_bottom = open("templates/bottom.html").read()
+    read_file = read_top + read_mid + read_bottom
+    open("docs/blog.html", "w+").write(read_file)
+    read_top = open("templates/top.html").read()
+    read_mid = open("content/projects.html").read()
+    read_bottom = open("templates/bottom.html").read()
+    read_file = read_top + read_mid + read_bottom
+    open("docs/projects.html", "w+").write(read_file)
+    read_top = open("templates/top.html").read()
+    read_mid = open("content/connect.html").read()
+    read_bottom = open("templates/bottom.html").read()
+    read_file = read_top + read_mid + read_bottom
+    open("docs/connect.html", "w+").write(read_file)
 
-read_top = open("templates/top.html").read()
-read_mid = open("content/projects.html").read()
-read_bottom = open("templates/bottom.html").read()
-read_file = read_top + read_mid + read_bottom
-open("docs/projects.html", "w+").write(read_file)
 
-read_top = open("templates/top.html").read()
-read_mid = open("content/connect.html").read()
-read_bottom = open("templates/bottom.html").read()
-read_file = read_top + read_mid + read_bottom
-open("docs/connect.html", "w+").write(read_file)
+
+
+
+
+
+if __name__ = '__main__':
+    main()
+
+
+
+
+
+# read_top = open("templates/top.html").read()
+# read_mid = open("content/index.html").read()
+# read_bottom = open("templates/bottom.html").read()
+# read_file = read_top + read_mid + read_bottom
+# open("docs/index.html", "w+").write(read_file)
+#
+# read_top = open("templates/top.html").read()
+# read_mid = open("content/blog.html").read()
+# read_bottom = open("templates/bottom.html").read()
+# read_file = read_top + read_mid + read_bottom
+# open("docs/blog.html", "w+").write(read_file)
+#
+# read_top = open("templates/top.html").read()
+# read_mid = open("content/projects.html").read()
+# read_bottom = open("templates/bottom.html").read()
+# read_file = read_top + read_mid + read_bottom
+# open("docs/projects.html", "w+").write(read_file)
+#
+# read_top = open("templates/top.html").read()
+# read_mid = open("content/connect.html").read()
+# read_bottom = open("templates/bottom.html").read()
+# read_file = read_top + read_mid + read_bottom
+# open("docs/connect.html", "w+").write(read_file)
