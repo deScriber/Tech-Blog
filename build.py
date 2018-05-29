9dceca8429f958771ad03dc4b7587a3490740dde
deScriber <tholsclaw@protonmail.com>
2018-05-29 13:28:46 -0700

    gotta keep em separated

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-page_name = [
+pages = [
     {
         "filename": "content/index.html",
         "title": "Tyler Holsclaw",
@@ -23,13 +23,28 @@ page_name = [
 
 
 def main():
-    for i in page_name:
+    for i in pages:
         read_template = open("templates/base.html").read()
         read_content = open(i["filename"]).read()
-        finish_page = read_template.replace("{{content}}", read_content).replace("{{title}}", i["title"])
-        open(i["filepath"], "w+").write(finish_page)
+        global add_content
+        add_content = read_template.replace("{{content}}", read_content)
+        return str(add_content)
+        # finish_page = read_template.replace("{{content}}", read_content).replace("{{title}}", i["title"])
+        # open(i["filepath"], "w+").write(finish_page)
 
 
+def templatify():
+    for i in pages:
+        global templated_content
+        templated_content = add_content.replace("{{title}}", i["title"])
+        return str(templated_content)
+
+
+def compilate():
+    for i in pages:
+        open(i["filepath"], "w+").write(templated_content)
 
 if __name__ == "__main__":
     main()
+    templatify()
+    compilate()
