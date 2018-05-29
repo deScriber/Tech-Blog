b1ba1c1f6a8d08b76df44639afa3ab7b1675bf8f
deScriber <tholsclaw@protonmail.com>
2018-05-29 08:43:47 -0700

    make more functions!

--- build.py
+++ build.py
@@ -22,10 +22,21 @@ page_name = [
 ]
 
 
-def main():
+def templateify():
     for i in page_name:
         read_template = open("templates/base.html").read()
+            return read_template
+
+
+def contentor():
+    for i in page_name:
         read_content = open(i["filename"]).read()
+            return read_content
+
+def main():
+    for i in page_name:
+        # read_template = open("templates/base.html").read()
+        # read_content = open(i["filename"]).read()
         finish_page = read_template.replace("{{content}}", read_content).replace("{{title}}", i["title"])
         open(i["filepath"], "w+").write(finish_page)
 
