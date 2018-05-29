b0e92831203080e58c3274f2b42bd4cc2eb2d764
deScriber <tholsclaw@protonmail.com>
2018-05-29 16:02:09 -0700

    it's working?!

--- build.py
+++ build.py
@@ -26,32 +26,37 @@ pages = [
 ]
 
 
+# def search_dictionaries(key, value, list_of_dictionaries):
+#     return [element for element in list_of_dictionaries if element[key] == value]
+
+
 def main():
     for i in pages:
-        global read_template
-        global read_content
         read_template = open("templates/base.html").read()
         read_content = open(i["filename"]).read()
         global add_content
         add_content = read_template.replace("{{content}}", read_content)
-    return add_content
+        i["templated_content"] = add_content
+        print(i["templated_content"])
+
         # finish_page = read_template.replace("{{content}}", read_content).replace("{{title}}", i["title"])
         # open(i["filepath"], "w+").write(finish_page)
 
 
-def templatify():
-    for i in pages:
-        global templated_content
-        templated_content = add_content.replace("{{title}}", i["title"])
-    return templated_content
+# def templatify(templated_content, add_content, pages):
+#     for i in pages:
+#         global templated_content
+#         templated_content = add_content.replace("{{title}}", i["title"])
+#         i["templated_content"] = templated_content
+#     return template_content
 
 
 # def compilate():
 #     for i in pages:
-#         open(i["filepath"], "w+").write(str(i["templated_content"]))
+#         open(i["filepath"], "w+").write(templated_content)
 
 if __name__ == "__main__":
     main()
-    templatify()
+    # templatify()
     # compilate()
-print(templated_content)
+# print(finish_page)
