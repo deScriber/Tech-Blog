67d77d0d1a42482d07c77f69b5f54215bb06123e
deScriber <tholsclaw@protonmail.com>
2018-06-05 16:11:08 -0700

    time to pip!

--- test.py
+++ test.py
@@ -2,54 +2,29 @@
 # all_html_files = glob.glob("content/*.html")
 # print(all_html_files)
 
-# import os
-# file_path = "content/index.html"
+import glob
+import os
+
+file_path = glob.glob("content/*.html")
+print(file_path)
 # file_name = os.path.basename(file_path)
 # print(file_name)
 # name_only, extension = os.path.splitext(file_name)
 # print(name_only)
 
-import glob
-import os
-
 
 site = {
     'base': 'templates/base.html',
-    'pages': [],
-}
+    'pages': [
 
-# pages =
-    #
-    # {
-    #     "filename": "content/index.html",
-    #     "title": "Tyler Holsclaw",
-    #     "filepath": "docs/index.html",
-    #     "templated_content": None,
-    # },
-    # {
-    #     "filename": "content/blog.html",
-    #     "title": "Logic at Work",
-    #     "filepath": "docs/blog.html",
-    #     "templated_content": None,
-    # },
-    # {
-    #     "filename": "content/projects.html",
-    #     "title": "Projects",
-    #     "filepath": "docs/projects.html",
-    #     "templated_content": None,
-    # },
-    # {
-    #     "filename": "content/connect.html",
-    #     "title": "Connect",
-    #     "filepath": "docs/connect.html",
-    #     "templated_content": None,
-    # }
+    ],
+}
 
 
 def find_page():
     all_html_files = glob.glob("content/*.html")
-    site['pages']['filename'] = [all_html_files]
-    print(str(site['pages']['filename']))
+    site['pages'] = [all_html_files]
+    print(site['pages'])
     # for page in site['pages']:
     #     print(str(site['pages']['filename'])
         # file_name = os.path.basename(file_path)
