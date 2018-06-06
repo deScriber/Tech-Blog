b509f5332d28bb013dc4e90915d8d2f055578c98
deScriber <tholsclaw@protonmail.com>
2018-06-06 13:14:45 -0700

    refactor jinja, os and glob

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-# import glob
-# all_html_files = glob.glob("content/*.html")
-# print(all_html_files)
-
 import glob
 import os
-
-# file_path = glob.glob("content/*.html")
-# print(file_path)
-# file_name = os.path.basename(file_path)
-# print(file_name)
-# name_only, extension = os.path.splitext(file_name)
-# print(name_only)
-
+from jinja2 import Template
+from datetime import datetime
 
 site = {
     'base': 'templates/base.html',
@@ -19,22 +9,58 @@ site = {
 }
 
 
-def find_page():
+def build_site():
     all_html_files = glob.glob("content/*.html")
     for page in all_html_files:
         file_name = os.path.basename(page)
+        gather_content(page, file_name)
+        # print(site['pages'])
         name_only, extension = os.path.splitext(file_name)
         name_only = name_only.capitalize()
-        if name_only == ("Index"):
-            name_only = ("Tyler Holsclaw")
-        site['pages'] = {"filename": str(page), "title": str(name_only), "filepath": "docs/"+str(file_name)}
-        print(site['pages'])
-        print(file_name)
-        print(page)
+        format_title(name_only)
+        print(name_only)
+# Formats Title Text
+        # name_only = name_only.capitalize()
+        # if name_only == ("Index"):
+        #     name_only = ("Tyler Holsclaw")
+        # if name_only == ("Blog"):
+        #     name_only = ("Logic at Work")
+# Reads content/template and templates pages.
+        # page_html = open(page).read()
+        # template_html = open(site['base']).read()
+        # template = Template(template_html)
+        # finished_page = template.render(
+        #     title = name_only,
+        #     content = page_html,
+        #     year = datetime.now().year
+        # )
+        # site['pages'] = {
+        #     "filename": str(page),
+        #     "filepath": "docs/"+str(file_name),
+        # }
+
+# Pulls filename/filepath and stores as key/value pairs in a dict.
+def gather_content(page, file_name):
+    site['pages'] = {
+        "filename": str(page),
+        "filepath": "docs/"+str(file_name),
+    }
+    return(site['pages'])
 
 
+    # Formats Title Text
+def format_title(name_only):
+    if name_only == ("Index"):
+        name_only = ("Tyler Holsclaw")
+    if name_only == ("Blog"):
+        name_only = ("Logic at Work")
+    return name_only
 
-
+        # print(finished_page)
+        # print(site['pages'])
+        # print(file_name)
+        # print(page)
+        # print(site['pages']['filepath'])
 
 
     # for page in site['pages']:
@@ -45,4 +71,4 @@ def find_page():
         # add_content = read_template.replace("{{content}}", read_content)
         # page["templated_content"] = add_content
         # print(add_content)
-find_page()
+build_site()
